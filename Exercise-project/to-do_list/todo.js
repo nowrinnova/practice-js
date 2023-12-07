8e9fedd0ad6a61a072b69cbb9b3097f4a706ebbd
nowrinnova <nowrinnova0088@gmail.com>
2023-12-07 23:11:51 +0600

    add

--- Exercise-project/to-do_list/todo.js
+++ Exercise-project/to-do_list/todo.js
@@ -1,12 +1,28 @@
-let item=['nova'];
+let array = ['meeting with rj group', 'finish all assingment'];
 
-function addTodo(){
-    let inputElement=document.querySelector('#todo-input');
-    let inputValue=inputElement.value;
-    let todoElement= document.querySelector('#todoItems');
-    // console.log(todoElement.innerText=inputValue);
-     item =todoElement.innerText=inputValue;
-    inputElement.value='';
-    console.log(item);
+        function clk() {
+            let inputElement = document.getElementById('input').value;
+            document.getElementById('input').value = '';
+            console.log(inputElement);
+            array.push(inputElement);
+            display();
+        }
 
-}
+
+        function display() {
+            let displayElement = document.getElementById('display');
+
+            let newHtml = '';
+            for (let i = 0; i < array.length; i++) {
+                newHtml +=
+                    `<div><span>${item}</span>
+                        <button class='btn-delete' onclick="array.splice(${i}, 1);
+                         display();">Delete</button>
+                </div>
+                `;
+                displayElement.innerHTML = newHtml;
+                // document.getElementById('display').innerText = array;
+            }
+
+
+        }
