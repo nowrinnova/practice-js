6ff88247bfc83d777de72416ab47507c5ad3804c
nowrinnova <nowrinnova0088@gmail.com>
2023-12-12 23:47:11 +0600

    add

--- mythra project/scripts/index.js
+++ mythra project/scripts/index.js
@@ -1,30 +1,21 @@
 let itemsContainerElement=document.querySelector('.items-container');
-let item={
-    item_image:'images/1.jpg',
-    rating:{
-        stars: 4.5,
-        noOfReviews:1400,
-
-    },
-    company_name:'calton London',
-    item_name:'Rodihum-plated CZ Floral Studes',
-    price:{
-        current_price:'Rs 606',
-        original_price:'Rs 1045',
-        discount:'(42% Off)',
-    },
-}
-itemsContainerElement.innerHTML =`<div class="item-container">
-<img class="item-img" src="${item.item_image}" alt="item-picture">
+let innerHTML='';
+items.forEach(item => {
+    innerHTML+=`<div class="item-container">
+<img class="item-img" src="${item.image}" alt="item-picture">
 <div class="rating">
-   ${item.rating.stars} ⭐ 5 | ${item.rating.noOfReviews}
+   ${item.rating.stars} ⭐ 5 | ${item.rating.count}
 </div>
-<div class="company-name">${item.company_name}</div>
+<div class="company-name">${item.company}</div>
 <div class="item-name">${item.item_name}</div>
 <div class="price">
-    <span class="current-price">${item.price.current_price}</span>
-    <span class="original-price">${item.price.original_price_price}</span>
-    <span class="discount">${item.price.discount}</span>
+    <span class="current-price">${item.current_price}</span>
+    <span class="original-price">${item.original_price}</span>
+    <span class="discount">(${item.discount_percentage}%OFF)</span>
 </div>
 <button class="btn-add-bag">Add to Cart</button>
-</div>`;
+</div>`
+    
+});
+
+itemsContainerElement.innerHTML =innerHTML;
